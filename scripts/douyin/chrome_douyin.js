e4dc9942bec9840c9b82e3fc89a8fc75be79ae54
ytx222 <ytx222@qq.com>
2023-11-26 17:36:59 +0800

    整理代码&esc适配&修改bug

--- scripts/douyin/chrome_douyin.js
+++ scripts/douyin/chrome_douyin.js
@@ -1,8 +1,8 @@
 console.log('ytx222--注入到抖音的js');
-const _topDocument = document;
 
 window.ytx222_download_douyin_video = ytx222_download_douyin_video;
 
+const _topDocument = document;
 const getVideo = (document = _topDocument) => {
 	const currentSwiper = document.querySelector(
 		`.swiper-slide.swiper-slide-active,[data-e2e="feed-active-video"],.feed-active-video`
@@ -11,6 +11,9 @@ const getVideo = (document = _topDocument) => {
 	const video = (currentSwiper ? currentSwiper : document).querySelector('.xg-video-container video');
 	return video;
 };
+
+let downloadIframe = null;
+
 async function ytx222_download_douyin_video(document = _topDocument, topDocument = _topDocument) {
 	window.tipsEl = window.tipsEl || null;
 	clearTips();
@@ -25,92 +28,18 @@ async function ytx222_download_douyin_video(document = _topDocument, topDocument
 		// 如果是blob
 		if (video.src.startsWith('blob:')) {
 			video.pause();
-			const newDIv = document.createElement('div');
-			// 屏幕顶部显示
-			newDIv.style.cssText = `
-				position: fixed;
-				top: 0px;
-				left: 0px;
-				width: 100vw;
-				height: 100vh;
-				background: rgba(0, 0, 0, 0.5);
-				z-index: -1;
-
-				display: flex;
-				justify-content: center;
-				align-items: center;
-
-			`;
-			newDIv.onclick = () => {
-				document.body.removeChild(newDIv);
-			};
-			const iframe = document.createElement('iframe');
-			iframe.width = '100%';
-			iframe.height = '100%';
-			iframe.style.transform = 'scale(0.85)';
-			iframe.style.transformOrigin = 'center center';
-			iframe.src = location.href;
-			iframe.onclick = e => {
-				e.stopPropagation();
-			};
-			// iframe.onload = async () => {
-
-			// 	// newWindow.addEventListener('load', async function () {
-			// 	// 	console.log('ytx222--newWindow load');
-			// 	// 	alert('11');
-			// 	// 	// newWindow.postMessage('ytx222_download_douyin_video', '*');
-
-			// 	// });
-			// 	// await newWindow.ytx222_download_douyin_video(topDocument);
-			// 	// document.removeChild(newDIv);
-			// 	console.log('ytx222--newWindow load');
-			// 	newWindow.postMessage('ytx222_download_douyin_video', '*');
-			// };
-			newDIv.appendChild(iframe);
-			document.body.appendChild(newDIv);
-			showTips('加载中...')
-			setTimeout(() => {
-				var newWindow = iframe.contentWindow;
-				console.log('iframe.contentWindow', iframe.contentWindow);
-				// 暂停播放视频逻辑
-				newWindow.addEventListener('DOMContentLoaded', async function () {
-					const noAutoplay = () =>
-						setTimeout(async () => {
-							const video = getVideo(newWindow.document);
-							if (video) {
-								video.autoplay = false;
-								video.onplay = () => {
-									video.pause();
-								};
-								// 直接把下载视频写到这里应该也行的
-								await ytx222_download_douyin_video(newWindow.document, topDocument);
-								document.body.removeChild(newDIv);
-							} else noAutoplay();
-						}, 4);
-					noAutoplay();
-				});
-				// newWindow.addEventListener('load', async function () {
-				// 	console.log('ytx222--newWindow load');
-
-				// });
-			}, 0);
-			// localStorage.setItem('ytx222_download_douyin_video_openWindow', location.href);
-			// var newWindow = window.open(location.href, '_blank', 'width=1000,height=800');
-			// newWindow.addEventListener('load', function () {
-			// 	console.log('ytx222--newWindow load');
-			// 	newWindow.postMessage('ytx222_download_douyin_video', '*');
-			// });
-
+			iframeDownload();
 			return;
 		}
 		const infoContainer = video.parentElement.parentElement.querySelector(
-			'.xgplayer-video-info-wrap .video-info-detail'
+			'.xgplayer-video-info-wrap .video-info-detail,#video-info-wrap '
 		);
-		let name = infoContainer?.querySelector('.account')?.innerText || '';
+		let name = infoContainer?.querySelector('.account .account-name,.account')?.innerText || '';
 		let desc = infoContainer?.querySelector('.title')?.innerText || '';
 		name = name.replace(/[\s@]/g, '');
 		desc = desc.replace(/[\s@]/g, '');
-		/** 在视频详细页可能找不到具体内容,就使用id+title */ if (name === '' && location.pathname) {
+		// 在视频详细页可能找不到具体内容,就使用id+title
+		if (name === '' && location.pathname) {
 			name = location.pathname.replace('/video/', '');
 			desc = document.title;
 		}
@@ -130,58 +59,133 @@ async function ytx222_download_douyin_video(document = _topDocument, topDocument
 		showTips('没有找到视频元素 ' + selectStr);
 		setTimeout(hideTips, 1000);
 	}
-	function downItem(url, fileName) {
-		return new Promise(r => {
-			var xhr = new XMLHttpRequest();
-			xhr.onreadystatechange = function () {
-				if (xhr.readyState === 4) {
-					if (xhr.status === 200) {
-						const a = document.createElement('a');
-						a.download = fileName;
-						a.href = URL.createObjectURL(xhr.response);
-						a.click();
-						r(1);
-					} else {
-						r(0);
-					}
+}
+
+function downItem(url, fileName) {
+	return new Promise(r => {
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					const a = document.createElement('a');
+					a.download = fileName;
+					a.href = URL.createObjectURL(xhr.response);
+					a.click();
+					r(1);
+				} else {
+					r(0);
 				}
-			};
-			xhr.responseType = 'blob';
-			xhr.open('GET', url);
-			xhr.send();
-		});
+			}
+		};
+		xhr.responseType = 'blob';
+		xhr.open('GET', url);
+		xhr.send();
+	});
+}
+function showTips(msg) {
+	tipsEl ??= document.createElement('div');
+	tipsEl.onclick ??= () => {
+		if (downloadIframe) downloadIframe.style.zIndex = 9999;
+	};
+	tipsEl.innerText = msg;
+	tipsEl.style.cssText =
+		'position: fixed;' +
+		'top: 0px;' +
+		'left: 0px;' +
+		'width: 100%;' +
+		'height: 75px;' +
+		'background: #029B62;' +
+		'z-index: 9999;' +
+		'color: #FFF;' +
+		'display: flex;' +
+		'justify-content: center;' +
+		'align-items: center;' +
+		'font-size: 30px;' +
+		'transition: opacity 2s;' +
+		'opacity: 1;';
+	document.body.appendChild(tipsEl);
+}
+function hideTips(msg) {
+	tipsEl.style.opacity = 0;
+	msg && (tipsEl.innerText = msg);
+	setTimeout(clearTips, 2000);
+}
+function clearTips() {
+	try {
+		document.body.removeChild(tipsEl);
+		window.tipsEl = null;
+	} catch {}
+}
+
+function iframeDownload () {
+	let url = location.href;
+	// 在第一层时,有时候会丢失参数,这里给他补上吧..
+	if (url === 'https://www.douyin.com/user/self?showTab=favorite_collection') {
+
+		const el=document.querySelector('[data-e2e-vid][data-e2e="feed-active-video"]')
+		if (el && el.dataset.e2eVid) {
+			url += '&video_id=' + el.dataset.e2eVid;
+		}
+
 	}
-	function showTips(msg) {
-		tipsEl ??= document.createElement('div');
-		tipsEl.innerText = msg;
-		tipsEl.style.cssText =
-			'position: fixed;' +
-			'top: 0px;' +
-			'left: 0px;' +
-			'width: 100%;' +
-			'height: 75px;' +
-			'background: #029B62;' +
-			'z-index: 9999;' +
-			'color: #FFF;' +
-			'display: flex;' +
-			'justify-content: center;' +
-			'align-items: center;' +
-			'font-size: 30px;' +
-			'transition: opacity 2s;' +
-			'opacity: 1;';
-		document.body.appendChild(tipsEl);
-	}
-	function hideTips(msg) {
-		tipsEl.style.opacity = 0;
-		msg && (tipsEl.innerText = msg);
-		setTimeout(clearTips, 2000);
-	}
-	function clearTips() {
-		try {
-			document.body.removeChild(tipsEl);
-			window.tipsEl = null;
-		} catch {}
-	}
+
+	const newDIv = document.createElement('div');
+	// 屏幕顶部显示
+	newDIv.style.cssText = `
+		position: fixed;
+		top: 0px;
+		left: 0px;
+		width: 100vw;
+		height: 100vh;
+		background: rgba(0, 0, 0, 0.5);
+		z-index: -1;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+	`;
+	newDIv.className = 'ytx222-iframe';
+
+	newDIv.onclick = () => {
+		document.body.removeChild(newDIv);
+	};
+	const iframe = document.createElement('iframe');
+	iframe.width = '100%';
+	iframe.height = '100%';
+	iframe.style.transform = 'scale(0.85)';
+	iframe.style.transformOrigin = 'center center';
+	iframe.src = location.href;
+	iframe.onclick = e => {
+		e.stopPropagation();
+	};
+
+	newDIv.appendChild(iframe);
+	document.body.appendChild(newDIv);
+	downloadIframe = newDIv;
+	showTips('加载中...');
+	setTimeout(() => {
+		var newWindow = iframe.contentWindow;
+		console.log('iframe.contentWindow', iframe.contentWindow);
+
+		const download = async () => {
+			// 直接把下载视频写到这里应该也行的
+			await ytx222_download_douyin_video(newWindow.document, topDocument);
+			document.body.removeChild(newDIv);
+			downloadIframe = null;
+		};
+		const stopVideo = () =>
+			setTimeout(async () => {
+				const video = getVideo(newWindow.document);
+				if (!video) return stopVideo();
+				// 暂停播放视频逻辑
+				video.autoplay = false;
+				video.onplay = () => video?.pause?.();
+				download();
+			}, 4);
+
+		newWindow.addEventListener('DOMContentLoaded', stopVideo);
+	}, 0);
 }
 
 // chrome.runtime.onStartup
@@ -194,6 +198,63 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 		ytx222_download_douyin_video();
 	}
 });
+/**
+ * esc
+ *
+ * 在第一层,parent #slidelist
+ * 在第二层 parent #upperFeedContainer
+ */
+window.addEventListener(
+	'keydown',
+	function (e) {
+		console.log('ytx222--按下了esc');
+		console.log(this.location.href);
+		if (e.key === 'Escape') {
+			const el2 = document.querySelector('#upperFeedContainer [data-e2e="feed-active-video"] video');
+			if (el2) return;
+			// setTimeout(() => {
+			// 	const el = document.querySelector('#slidelist [data-e2e="feed-active-video"] video');
+			// 	if (el) {
+			// 		el.pause();
+			// 		// el.parentElement.parentElement.parentElement.parentElement.paren
+			// 	}
+			// }, 100);
+
+			if (location.href === 'https://www.douyin.com/user/self?showTab=favorite_collection') {
+				// 关闭当前视频
+				// 抖音bug,反正是代码报错了
+				console.log('ytx222--执行关闭弹窗');
+				document.querySelectorAll('.isDark > svg,.ZCHTRJzJ')[0].click();
+			}
+
+			// if (el || el2) {
+			// 	e.preventDefault();
+			// 	e.stopPropagation();
+			// 	return;
+			// }
+		}
+	},
+	true
+);
+
+function* ElementParentIterator(el) {
+	let curEl = el;
+	while (curEl) {
+		yield curEl;
+		curEl = curEl?.parentElement;
+	}
+	return null;
+}
+/**
+ * for (const item of ElementParentIterator(e.target)) {
+			console.log(item);
+			if (item === el.themeContainer) return;
+			// if (item.classList.contains("remove-icon"))
+			//     return void deleteItem(item.parentElement);
+			if (item.classList.contains('add-theme')) return addTheme();
+			if (item.classList.contains('theme-item')) return void clickItem(item);
+		}
+ */
 
 // window.addEventListener('message', async function (e) {
 // 	console.log('ytx222--接收到message');
